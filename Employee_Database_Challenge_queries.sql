78cc05893969a20b24a3ce7fbc2fe9fa72b527b3
cherylberger <94234511+cherylberger@users.noreply.github.com>
2022-01-16 23:25:25 -0500

    Add files via upload
    Update Employee_Database_challenge_queries.sql to show code for Module 7 challenge Deliverables 1 and 2

--- Employee_Database_Challenge_queries.sql
+++ Employee_Database_Challenge_queries.sql
@@ -83,3 +83,78 @@ FROM unique_titles
 
 SELECT * FROM retiring_titles
 ORDER BY count DESC
+
+--Deliverable 2 - The Employees Eligible for the Mentorship Program
+-- Create Department Employees table
+CREATE TABLE dept_emp (
+	 emp_no INT NOT NULL,
+     dept_no VARCHAR(4) NOT NULL,
+	 from_date DATE NOT NULL, 
+	 to_date DATE NOT NULL,
+     FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+	
+SELECT * FROM dept_emp
+	
+-- Create new table for current employees
+SELECT employees.emp_no,
+     employees.first_name,
+     employees.last_name,
+	 employees.birth_date, 
+	 dept_emp.from_date,
+	 dept_emp.to_date
+INTO current_employees
+FROM employees 
+INNER JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+ORDER BY employees.emp_no;
+		
+SELECT * FROM current_employees
+
+--Create new table for eligible_titles
+SELECT employees.emp_no,
+     employees.first_name,
+     employees.last_name,
+	 employees.birth_date, 
+	 titles.title,
+	 titles.from_date,
+	 titles.to_date
+INTO eligible_titles
+FROM employees 
+INNER JOIN titles
+ON employees.emp_no = titles.emp_no
+ORDER BY employees.emp_no;
+		
+SELECT * FROM eligible_titles	
+	
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+birth_date,
+title, 
+from_date
+to_date
+INTO eligible_employees
+FROM eligible_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no, title DESC;
+
+SELECT * FROM eligible_employees;
+	
+--Create new table for membership eligibility
+SELECT eligible_employees.emp_no, 
+	eligible_employees.first_name, 
+	eligible_employees.last_name,
+	eligible_employees.birth_date,
+	eligible_titles.title,
+	eligible_titles.from_date,
+	eligible_titles.to_date
+INTO membership_eligibility
+FROM eligible_employees
+INNER JOIN eligible_titles
+ON eligible_employees.emp_no = eligible_titles.emp_no
+WHERE eligible_employees.birth_date BETWEEN ('1965-01-01') AND ('1965-12-31')
+ORDER BY eligible_titles.emp_no ASC, eligible_employees.birth_date DESC; 
+
+SELECT * FROM membership_eligibility;
